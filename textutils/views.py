3dd790bb55fd278fc291ef1dc611b58f410c6121
kunaldesign <kunalhedaoo25@gmail.com>
2021-11-26 03:16:38 -0500

    v.2.1.2

--- textutils/views.py
+++ textutils/views.py
@@ -32,28 +32,34 @@ def analyze(request):
     
     #Check which checkbox is on
     #analyze the text
-    if removepunc == "on":
+    if(removepunc == "on"):
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif(fullcaps=="on"):
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if(fullcaps=="on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
         params = {'purpose': 'Chanaged into uppercase', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif(newlineremover=="on"):
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if(newlineremover=="on"):
         analyzed = ""
         for char in djtext:
-            if char != "\n":
+            if char != "\n" and char != "\r":
                 analyzed = analyzed + char
+            else:
+                print("no")
+            print("pre",analyzed)
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif(extraspaceremover=="on"):
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if(extraspaceremover=="on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if djtext[index] == " " and djtext[index+1]==" ":
@@ -61,10 +67,11 @@ def analyze(request):
             else:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
-
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    #else:
+        #return HttpResponse("Error")
+    return render(request, 'analyze.html', params)
 
 # def capfirst(request):
 #     return HttpResponse("capitalize first")
